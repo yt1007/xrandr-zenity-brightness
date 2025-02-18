d300c3d6b6593c310f4fa0f8d5f04b411876f6a6
Y T <yealingt@42iskandarputeri.edu.my>
2025-02-18 19:48:05 +0800

    Added BASH script that: 1. Runs xrandr to identify connected primary display 2. Runs xrandr to identify current brightness (0-1) for the primary display. 3. Runs zenity to present a slider to the user that runs xrandr to set the brightness for the primary display.

---
+++ brightness.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+
+## Identify primary display
+PrimaryScreen=$(/usr/bin/xrandr --current | \
+	/usr/bin/grep -w connected | \
+	/usr/bin/cut -f 1 -d " ")
+
+if [[ ! "${PrimaryScreen}" ]];
+then
+	echo "Failed to identify primary display. Closing brightness app." >&2
+	exit $?
+fi;
+
+## Identify current Brightness value
+CurrentBrightness=$(/usr/bin/xrandr --current --verbose | \
+	/usr/bin/sed -n "/ connected primary /,/ disconnected / p" | \
+	/usr/bin/grep -wi brightness | \
+	/usr/bin/sed "s/^.*: //")
+CurrentBrightness=$(echo "${CurrentBrightness} * 100 / 1" | /usr/bin/bc)
+
+if [[ ${CurrentBrightness} -lt 0 || ${CurrentBrightness} -gt 100 ]];
+then
+	echo "Failed to identify primary display. Closing brightness app." >&2
+	exit $?
+fi;
+
+## Draw a Zenity Window to Adjust Brightness
+/usr/bin/zenity --scale \
+	--text="Brightness" \
+	--value=${CurrentBrightness} \
+	--min-value=0 \
+	--max-value=100 \
+	--print-partial | \
+	while read BrightPercent;
+	do
+		BrightValue=$(echo "${BrightPercent} * 0.01" | /usr/bin/bc)
+		/usr/bin/xrandr --output ${PrimaryScreen} \
+			--brightness ${BrightValue}
+	done
+
+## Quit
+exit $?
